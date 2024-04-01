db542af212ad9926b29b28248f109706f98596a9
umar-abdullah22 <umer4361@gmail.com>
2024-04-02 02:18:39 +0500

    navbar structure fixed

---
+++ src/components/Navbar/constant.ts
@@ -0,0 +1,29 @@
+export const menu = [
+    {
+        key: 1,
+        label: "About",
+    },
+    {
+        key: 2,
+        label: "Portfolio",
+    },
+    {
+        key: 3,
+        label: "Contact",
+    },
+];
+
+export const socialMenu = [
+    {
+        key: 1,
+        label: "Facebook",
+    },
+    {
+        key: 2,
+        label: "Instagram",
+    },
+    {
+        key: 3,
+        label: "LinkedIn",
+    },
+];
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import Logo from '../../assets/logo.png'
+import { menu } from './constant';
+import { socialMenu } from './constant';
+import Ic_Facebook from '../../assets/svg/ic_facebook.svg'
+
 
 const Navbar = () => {
     return (
         <>
-            <div className='w-full h-20 bg-black px-20 flex items-center'>
+            <div className='w-full bg-black px-20 py-3 flex items-center justify-between' style={{ background: 'linear-gradient(90deg, rgba(11,168,243,1) 0%, rgba(5,106,162,1) 50%, rgba(11,168,243,1) 100%)' }}>
+                <div>
+                <ul className='flex items-center gap-5'>
+                        {socialMenu.map(item => (
+                            <li className='text-white' key={item.key}>{item.label}</li>
+                        ))}
+                    </ul>
+                </div>
                 <div>
                     <img src={Logo} alt="Logo" className='h-16 w-16 rounded-full' />
                 </div>
                 <div>
-                    
+                    <ul className='flex items-center gap-5'>
+                        {menu.map(item => (
+                            <li className='text-white' key={item.key}>{item.label}</li>
+                        ))}
+                    </ul>
                 </div>
             </div>
         </>
