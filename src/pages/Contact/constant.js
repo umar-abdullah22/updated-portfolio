c539eb50fefcd9006cd23e99228ae779bd7c2210
Umar Farooq <124269624+umaaarf8@users.noreply.github.com>
2024-04-20 18:58:32 +0500

    contact page

--- src/pages/Contact/constant.js
+++ src/pages/Contact/constant.js
@@ -3,6 +3,13 @@ import InstagramIcon from "../../assets/Images/Socials/4.png"
 import LinkedInIcon from "../../assets/Images/Socials/2.png"
 import MailIcon from "../../assets/Images/Socials/3.png"
 
+export const HeroSectionContent = [
+    {
+        title: "Contact",
+        subTitle: "Get in touch with me via social media or email."
+    }
+]
+
 export const Socials = [
     {
         key: 1,
