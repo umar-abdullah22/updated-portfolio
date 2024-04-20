14cb94c2652582d9638ae88b9eb636247c2f0079
Umar Farooq <124269624+umaaarf8@users.noreply.github.com>
2024-04-20 22:06:08 +0500

    changes are complete

--- src/pages/Portfolio/constant.js
+++ src/pages/Portfolio/constant.js
@@ -1,43 +1,52 @@
+import sumTube from '../../assets/Images/sumTube.png'
+import lineUpPolo from '../../assets/Images/lineUpPolo.png'
+import lieBeVaLo from '../../assets/Images/lieBeVaLo.png'
+import topPem from '../../assets/Images/TopPem.png'
+import Agentur from '../../assets/Images/Agentur.png'
+import BillSupply from '../../assets/Images/BillSupply.png'
+import MedCardsNow from '../../assets/Images/MedCardsNow.png'
+import SyrusPos from '../../assets/Images/SyrusPos.png'
+
 export const CardContent = [
     {
-        CardImage: "https://picsum.photos/200/300",
-        Title: "Lizard",
+        CardImage: sumTube,
+        Title: "SumTube",
+        Description: "SumTube AI, a cutting-edge YouTube video summarizer. With lightning speed, it extracts text summaries for any video. Empower your content curation effortlessly!",
+    },
+    {
+        CardImage: lineUpPolo,
+        Title: "LineUpPolo",
+        Description: "Lineup Polo, founded by Alice & Rosanna, revolutionizes polo management with a free app & web portal, automating tournaments & fostering community.",
+    },
+    {
+        CardImage: lieBeVaLo,
+        Title: "LieBevalo",
+        Description: "Liebevalo, your go-to dating app for effortless connections. With its intuitive interface and innovative features, finding love has never been easier.",
+    },
+    {
+        CardImage: topPem,
+        Title: "TopPem",
+        Description: "Passionate team merges sports, eSports, blockchain for interactive global experiences. Sustainable, fair, transparent gaming revolutionizes entertainment.",
+    },
+    {
+        CardImage: Agentur,
+        Title: "Agentur",
         Description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
     },
     {
-        CardImage: "https://picsum.photos/200/300",
-        Title: "Lizard",
-        Description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+        CardImage: BillSupply,
+        Title: "BillSupply",
+        Description: "Transforming business and employment with an integrated platform. Leading in service administration and payments. Values: God, Family, Innovation.",
     },
     {
-        CardImage: "https://picsum.photos/200/300",
-        Title: "Lizard",
-        Description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+        CardImage: MedCardsNow,
+        Title: "MedCardsNow",
+        Description: "Seamless access to medical cannabis cards. Connect with licensed professionals, rapid approval, curated dispensaries. Confidential support.",
     },
     {
-        CardImage: "https://picsum.photos/200/300",
-        Title: "Lizard",
-        Description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-    },
-    {
-        CardImage: "https://picsum.photos/200/300",
-        Title: "Lizard",
-        Description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-    },
-    {
-        CardImage: "https://picsum.photos/200/300",
-        Title: "Lizard",
-        Description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-    },
-    {
-        CardImage: "https://picsum.photos/200/300",
-        Title: "Lizard",
-        Description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-    },
-    {
-        CardImage: "https://picsum.photos/200/300",
-        Title: "Lizard",
-        Description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+        CardImage: SyrusPos,
+        Title: "SyrusPos",
+        Description: "Your one-stop solution for POS machines. Streamline transactions, manage payments effortlessly. All-inclusive service, tailored for your business needs.",
     },
     
 ]
