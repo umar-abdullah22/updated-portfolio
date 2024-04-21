39c24ca0a19408fa48d347b0be5d338ca8d05c62
Umar Farooq <124269624+umaaarf8@users.noreply.github.com>
2024-04-21 11:00:56 +0500

    ui complete

---
+++ src/pages/Experience/constant.js
@@ -0,0 +1,41 @@
+export const EducationContent = [
+    {
+        key: 1,
+        year: "2018 - 2022",
+        institute: "High School Degree",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+    },
+    {
+        key: 2,
+        year: "2018 - 2022",
+        institute: "Bachelor's Degree",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+    },
+    {
+        key: 3,
+        year: "2018 - 2022",
+        institute: "Master's Degree",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+    }
+];
+
+export const ExperienceContent = [
+    {
+        key: 1,
+        year: "2018 - 2022",
+        institute: "High School Degree",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+    },
+    {
+        key: 2,
+        year: "2018 - 2022",
+        institute: "Bachelor's Degree",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+    },
+    {
+        key: 3,
+        year: "2018 - 2022",
+        institute: "Master's Degree",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+    }
+];
--- src/pages/Experience/index.jsx
+++ src/pages/Experience/index.jsx
@@ -1,55 +1,41 @@
 import React from 'react'
 import { ExperienceStyle } from './style.js'
+import { EducationContent } from './constant.js'
+import { ExperienceContent } from './constant.js'
 
 const Experience = () => {
     return (
         <>
             <ExperienceStyle>
-                <div class="container">
-                    <main class="row">
-                        <section class="col">
-                            <header class="title">
+                <div className="container">
+                    <main className="row">
+                        <section className="col">
+                            <header className="title">
                                 <h2>EDUCATION</h2>
                             </header>
-                            <div class="contentBox">
-                                <div class="box">
-                                    <h4>2018 - 2022</h4>
-                                    <h3>High School Degree</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                                </div>
-                                <div class="box">
-                                    <h4>2018 - 2022</h4>
-                                    <h3>Bachelor's Degree</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                                </div>
-                                <div class="box">
-                                    <h4>2018 - 2022</h4>
-                                    <h3>Master Degree</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                                </div>
+                            <div className="contentBox">
+                                {EducationContent.map(item => (
+                                    <div key={item.key} className="box">
+                                        <h4>{item.year}</h4>
+                                        <h3>{item.institute}</h3>
+                                        <p>{item.description}</p>
+                                    </div>
+                                ))}
                             </div>
                         </section>
 
-                        <section class="col">
-                            <header class="title">
+                        <section className="col">
+                            <header className="title">
                                 <h2>EXPERIENCE</h2>
                             </header>
-                            <div class="contentBox">
-                                <div class="box">
-                                    <h4>2018 - 2022</h4>
-                                    <h3>UI/UX Designer</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                                </div>
-                                <div class="box">
-                                    <h4>2018 - 2022</h4>
-                                    <h3>Full-Stack Developer</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                                </div>
-                                <div class="box">
-                                    <h4>2018 - 2022</h4>
-                                    <h3>Frontend Developer</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                                </div>
+                            <div className="contentBox">
+                                {ExperienceContent.map(item => (
+                                    <div key={item.key} className="box">
+                                        <h4>{item.year}</h4>
+                                        <h3>{item.institute}</h3>
+                                        <p>{item.description}</p>
+                                    </div>
+                                ))}
                             </div>
                         </section>
                     </main>
